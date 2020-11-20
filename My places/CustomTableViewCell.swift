4724b4e13512aeb6cf3eaf2f49a847151192a745
Victor Kalashnik <kalashnikvictor@gmail.com>
2020-11-20 15:03:27 +0300

    Custom Cell

---
+++ My places/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  My places
+//
+//  Created by Виктор Калашник on 20.11.2020.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+}
